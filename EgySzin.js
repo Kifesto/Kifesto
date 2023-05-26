0cb002c816b92ea8a29f87811ecbe5ad47afaebc
fureszb <115637991+fureszb@users.noreply.github.com>
2023-05-26 13:40:10 +0200

    Update EgySzin.js

--- EgySzin.js
+++ EgySzin.js
@@ -1,25 +1,26 @@
-class Szin{
-    #id;
-    #szin;
-    constructor(id, szin, szuloElem) {
-       this.#szin = szin;
-        this.#id = id;
-        this.szuloElem = szuloElem.append(`<div class="szin" id="${this.#id}" style="background-color: ${this.#szin};">&nbsp</div>`);
-        this.elem = $(".szin:last-child");
-        this.elem.on("click", ()=>{
-            console.log("szín kiválasztva: "+this.#id+" - "+this.#szin)
-        });
+class Szin {
+  #id;
+  #szin;
+  constructor(id, szin, szuloElem) {
+    this.#szin = szin;
+    this.#id = id;
+    this.szuloElem = szuloElem.append(`<div class="szin" id="${this.#id}" style="background-color: ${this.#szin};">&nbsp</div>`);
+    this.elem = $(".szin:last-child");
+    this.elem.on("click", () => {
+      console.log("szín kiválasztva: " + this.#id + " - " + this.#szin)
+      this.kattintasTrigger()
+    });
 
-
-    }
-    setColor(e) {
-        const colorPicker = $(e.target);
-        if (colorPicker.hasClass('szin')) {
-          const color = colorPicker.css('background-color');
-          this.negyzetek.forEach(negyzet => {
-            negyzet.color = color;
-          });
-        }
+  }
+  getSzin() {
+    this.#szin = this.elem.css("background-color");
+    return this.#szin;
+    
+  }
+  kattintasTrigger() {
+        const esemeny=new CustomEvent("szinValasztas", {detail:this.#szin})
+        window.dispatchEvent(esemeny)
+    
       }
 };
 export default Szin;
