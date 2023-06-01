41542f3f0c4a9ff9ca7a7afe0f9d11e62391abd0
fureszb <fureszb@gmail.com>
2023-06-01 14:21:49 +0200

    articleSzelesseg

--- KepMegjelenít.js
+++ KepMegjelenít.js
@@ -1,37 +1,37 @@
 class KepMegjelenit {
-    #id
-    constructor(szuloElem,id,azonosito,kokckaElem,szinElem) {
-        this.#id=id;
-        this.azonosito=azonosito;
-        this.szuloElem = szuloElem.append(`<div class="kep" id="${this.azonosito}"><img src="${this.#id}" alt=kep></div>`);
-        this.elem = $(".kep:last-child");
-        this.elem.on("click", ()=>{
-            this.szuloElem.children(".kep").not(this.elem).hide();
-            this.elem.addClass("kivalasztottKep");
-            this.elem.removeClass("kep");
-            kokckaElem.show();
-            szinElem.show(); 
-            
-        });
-        //const kepEleres = $(".image");
-        //let txt = kivalasztas;
-        //kepEleres.append(txt);
-        //this.imgELem = kepEleres.children("img");
-    }
-    getWidth() {
+  #id;
+  constructor(szuloElem, id, azonosito, kokckaElem, szinElem) {
+    this.#id = id;
+    this.azonosito = azonosito;
+    this.szuloElem = szuloElem.append(
+      `<div class="kep" id="${this.azonosito}"><img src="${
+        this.#id
+      }" alt=kep></div>`
+    );
+    this.elem = $(".kep:last-child");
+    this.elem.on("click", () => {
+      this.szuloElem.children(".kep").not(this.elem).hide();
+      this.elem.addClass("kivalasztottKep");
+      this.elem.removeClass("kep");
+      kokckaElem.show();
+      szinElem.show();
+      console.log(this.getWidth(this.elem));
+    });
+  }
 
-
-        // Kép betöltődése után a szélesség lekérése
-        this.imgELem.on("load", () => {
-            const szeleseg = this.imgELem.width();
-            console.log(szeleseg);
-            return szeleseg;
-        });
-
-    }
-    kivalasztas(){
-
-    }
-
+  getWidth(konkKep) {
+    const szelesseg = konkKep.width();
+    const magassag = konkKep.height();
+    const szazalekosSzelesseg =
+      (szelesseg / konkKep.parent().width()) * 150;
+    const articleElem = $("article");
+    articleElem.css("width", szazalekosSzelesseg + "%");
+    articleElem.css("height", magassag  + "%");
+    const esemeny = new CustomEvent("kepSzelesseg", {
+      detail: szelesseg,
+    });
+    window.dispatchEvent(esemeny);
+  }
 }
+
 export default KepMegjelenit;
